7c03a98b366324b694e13375cf0cbca533bc6a1b
Erik Dungan <erikdungan@gmail.com>
2025-01-07 22:24:12 -0600

    cleanup naming and package manifest

--- Package.swift
+++ Package.swift
@@ -9,16 +9,17 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "Rational",
-            targets: ["Rational", "IntExtensions"]),
+            targets: ["Rational"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Rational"),
+            name: "Rational"
+        ),
         .testTarget(
             name: "SwiftRationalTests",
-            dependencies: ["Rational", "IntExtensions"]
+            dependencies: ["Rational"]
         ),
     ]
 )
